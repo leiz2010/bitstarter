5e9e2e4cb1aee2133439039ecee35364f3fdf201
leiz2010 <leiz2010@gmail.com>
2013-07-14 00:56:51 +0000

    two more time

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 //var buffer = new Buffer(16);
 //buffer = fs.readFileSync('index.html');
+var buffer = new Buffer ("what the heck!", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send('ahhhhh!!!!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
