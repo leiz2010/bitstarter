f241638e5ff4e5c93b4544669b10571be0149845
leiz2010 <leiz2010@gmail.com>
2013-07-14 01:02:46 +0000

    third time

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 //var buffer = new Buffer(16);
-//buffer = fs.readFileSync('index.html');
-var buffer = new Buffer ("what the heck!", "utf-8");
+var buffer = fs.readFileSync('./index.html');
+//var buffer = new Buffer ("what the heck!", "utf-8");
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
