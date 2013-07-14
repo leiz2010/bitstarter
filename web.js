5eef6bbad971ef4d2b2adb52d07bd8efd28af4e5
leiz2010 <leiz2010@gmail.com>
2013-07-14 00:49:39 +0000

    correct buffer

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buffer = fs.readFileSync('index.html');
+var buffer = new Buffer(16);
+buffer = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
