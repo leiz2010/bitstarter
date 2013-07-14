1be5d1e99553bd49e8a48497cac11177fa216bf5
leiz2010 <leiz2010@gmail.com>
2013-07-14 01:15:41 +0000

    5 times

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-//var buffer = new Buffer(16);
-var str = fs.readFileSync('index.html','utf-8');
+var fs = require('fs');
+var buf = fs.readFileSync('index.html');
 //var buffer = new Buffer ("what the heck!", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(str);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
