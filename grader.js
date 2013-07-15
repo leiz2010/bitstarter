48bcd482a0cb81751e5b3ea3b286a1f561139b3a
leiz2010 <leiz2010@gmail.com>
2013-07-15 01:19:09 +0000

    URl enabled

--- grader.js
+++ grader.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 var fs = require('fs');
+var rest = require("restler");
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT ="http://glacial-stream-7592.herokuapp.com/";
 
 var assertFileExists = function(infile){
     var instr = infile.toString();
@@ -16,6 +18,7 @@ var assertFileExists = function(infile){
 
 var cheerioHtmlFile = function(htmlfile){
     return cheerio.load(fs.readFileSync(htmlfile));
+   
 };
 
 var loadChecks = function(checksfile){
@@ -33,6 +36,7 @@ var checkHtmlFile = function(htmlfile, checksfile){
     return out;
 };
 
+
 var clone = function(fn){
     //Workaround for comander.js issue.
     //http://stackoverflow.com/a/6772648
@@ -43,7 +47,13 @@ if(require.main == module){
     program 
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_link>','path to url', URL_DEFAULT)
+
     .parse(process.argv);
+    
+	rest.get(URL_DEFAULT).on('complete',function(result){
+	    program.file = result;
+	});
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
